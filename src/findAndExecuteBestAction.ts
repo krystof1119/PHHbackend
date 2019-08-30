e48dbfc9b206d54efdbb27ffdb96b05d93d80c65
Kryštof Píštěk <krystof1119@gmail.com>
2019-08-30 20:57:31 +0200

    Create the findAndExecuteBestFunction function

---
+++ src/findAndExecuteBestAction.ts
@@ -0,0 +1,13 @@
+import * as stringSimilarity from 'string-similarity';
+import keywords from "./keywords";
+import {BestMatch} from "string-similarity";
+
+export default async function findAndExecuteBestAction(val: string): Promise<string> {
+    let matches: BestMatch = stringSimilarity.findBestMatch(val, Object.keys(keywords));
+    if (matches.bestMatch.rating < 0.25) {
+        return '';
+    }
+    let bestKeyword : string = matches.bestMatch.target;
+    let bestFunction : Function = keywords[bestKeyword];
+    return bestFunction(val);
+}
