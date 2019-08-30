ff24d732ceecf40391471d63f9c3d029f08da9a6
Kryštof Píštěk <krystof1119@gmail.com>
2019-08-30 20:57:56 +0200

    Use actual algorithms for the resolver

--- src/graphql/resolvers/dataResolver.ts
+++ src/graphql/resolvers/dataResolver.ts
@@ -1,10 +1,11 @@
 import {Arg, Query, Resolver} from "type-graphql";
 import Data from "../types/data";
+import findAndExecuteBestAction from "../../findAndExecuteBestAction";
 
 @Resolver(Data)
 export default class DataResolver {
     @Query(() => String)
     async res(@Arg('var') val: string): Promise<string> {
-        return 'test';
+        return findAndExecuteBestAction(val);
     }
 }
