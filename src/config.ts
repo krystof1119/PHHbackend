10600c952bdc8c1b7b5cc601c3b38e8417d55f73
krystof1119 <35891551+krystof1119@users.noreply.github.com>
2019-08-30 19:35:48 +0200

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export default {
+    PORT: <number> 7589,
+};
---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import 'reflect-metadata';
+import * as express from 'express';
+import CONFIG from './config';
+import {buildSchema} from "type-graphql";
+import {GraphQLSchema} from "graphql";
+import * as graphqlHTTP from "express-graphql";
+
+async function main(): Promise<void> {
+    let schema : GraphQLSchema = await buildSchema({
+        resolvers: []
+    });
+
+
+
+    const app: express.Application = express();
+
+    app.use('/graphql', graphqlHTTP({schema, graphiql: true}));
+
+    app.listen(CONFIG.PORT, () => {console.log(`Server started on port ${CONFIG.PORT}`)})
+
+}
+
+main().then((): void => {});
