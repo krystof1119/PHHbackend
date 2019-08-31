b1d52b7df2854f7026177eb616a3ebe9e8bcfa4a
Kryštof Píštěk <krystof1119@gmail.com>
2019-08-31 21:17:53 +0200

    If user is in zork mode, just handleZork instead of executing the best action.

--- src/graphql/resolvers/dataResolver.ts
+++ src/graphql/resolvers/dataResolver.ts
@@ -1,11 +1,16 @@
 import {Arg, Query, Resolver} from "type-graphql";
 import Data from "../types/data";
 import findAndExecuteBestAction from "../../findAndExecuteBestAction";
+import isInZork from "../../zork/isInZork";
+import handleZork from "../../zork/handleZork";
 
 @Resolver(Data)
 export default class DataResolver {
     @Query(() => String)
     async res(@Arg('var') val: string, @Arg('browserId') browserId: string): Promise<string> {
+        if (isInZork(browserId)) {
+            return handleZork(val, browserId);
+        }
         return findAndExecuteBestAction(val, browserId);
     }
 }
